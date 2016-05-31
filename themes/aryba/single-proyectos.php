816aed64d172686ba68f3878f420744135e68eba
UlfAschmann <ulf.aschmann@gmail.com>
2016-05-31 12:26:51 -0500

    cambios css / logo / php proyectos - desarollos

--- themes/aryba/single-proyectos.php
+++ themes/aryba/single-proyectos.php
@@ -1,2 +1,111 @@
-Aqui van los single
+<?php get_header(); ?>
 
+	<!-- content-holder  -->
+	<div class="content-holder">
+		<!-- Page title -->
+		<div class="dynamic-title">Proyecto Individual</div>
+		<!-- Page title  end-->
+		<!-- content  -->
+		<!-- content  -->
+		<div class="content">
+			<section class="parallax-section">
+				<div class="parallax-inner">
+					<div class="bg" data-bg="images/bg/1.jpg" data-top-bottom="transform: translateY(300px);" data-bottom-top="transform: translateY(-300px);"></div>
+					<div class="overlay"></div>
+				</div>
+				<div class="container">
+					<div class="page-title">
+						<div class="row">
+							<div class="col-md-6">
+								<h2>Project <strong> title </strong></h2>
+							</div>
+							<div class="col-md-6">
+								<ul class="creat-list">
+									<li><a href="index.html">Home</a></li>
+									<li><a href="portfolio4.html">Portfolio</a></li>
+									<li><a href="portfolio-single5.html">Portfolio single</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+		<!-- content end -->
+		<div class="content">
+			<section>
+				<div class="container">
+					<div class="single-slider-holder lightgallery" data-looped="0">
+						<div class="single-slider" data-loppsli="0">
+							<!-- 1 -->
+							<div class="item">
+								<img src="images/folio/thumbs/1.jpg" alt="">
+								<a data-src="images/folio/thumbs/1.jpg" class="popup-image slider-zoom" data-sub-html="alteration in some">
+									<i class="fa fa-expand"></i>
+								</a>
+							</div>
+							<!-- 1 end-->
+							<!-- 2 -->
+							<div class="item">
+								<img src="images/folio/thumbs/1.jpg" alt="">
+								<a data-src="images/folio/thumbs/1.jpg" class="popup-image slider-zoom" data-sub-html="many variations">
+									<i class="fa fa-expand"></i>
+								</a>
+							</div>
+							<!-- 2 end-->
+							<!-- 3 -->
+							<div class="item">
+								<img src="images/folio/thumbs/1.jpg" alt="">
+								<a data-src="images/folio/thumbs/1.jpg" class="popup-image slider-zoom" data-sub-html="majority have">
+									<i class="fa fa-expand"></i>
+								</a>
+							</div>
+							<!-- 3 end  -->
+						</div>
+						<div class="customNavigation ssn">
+							<a class="prev-slide transition"><i class="fa fa-angle-left"></i></a>
+							<a class="next-slide transition"><i class="fa fa-angle-right"></i></a>
+						</div>
+					</div>
+					<!-- single slider  end -->
+					<div class="custom-inner-holder">
+						<!-- 1 -->
+						<div class="custom-inner">
+							<div class="row">
+								<div class="col-md-6">
+									<h3>Project details</h3>
+								</div>
+								<div class="col-md-6">
+									<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words</p>
+
+									<div class="espacio"></div>
+
+									<h4 class="lista-single">Lorem Ipsum available</h4>
+									<p>Door portals</p>
+
+									<h4 class="lista-single">Lorem Ipsum available</h4>
+									<p>Door portals</p>
+
+									<h4 class="lista-single">Lorem Ipsum available</h4>
+									<p>Door portals</p>
+
+									<div class="content-nav">
+										<ul>
+											<li><a href="portfolio-single2.html" class=" text-link">next project</a></li>
+											<li><a href="portfolio-single3.html" class=" text-link">prev project</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- 1 -->
+					</div>
+					<!-- custom-link-holder  end -->
+				</div>
+			</section>
+		</div>
+		<!-- content end -->
+
+
+
+		<?php get_footer(); ?>
