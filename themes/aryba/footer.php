28622a605bb133277c81f2fa0bb3f6f03fe957ad
Rogelio <rogelio@dsindigo.mx>
2016-06-29 12:09:06 -0500

    Ajustes para mejor funcionamiento

--- themes/aryba/footer.php
+++ themes/aryba/footer.php
@@ -1,125 +1,119 @@
-<?php if (!is_post_type_archive('proyectos') && !is_404()): ?>
-	<!-- content footer-->
-	<div class="height-emulator"></div>
-	<footer class="content-footer">
-		<!--  container  -->
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6">
-					<!-- Footer logo -->
-					<!--<div class="footer-item footer-logo">
-	<a href="index.html"><img src="images/footer-logo.png" alt=""></a>
-	<p> </p>
-	</div>-->
-					<!-- Footer logo end -->
-				</div>
-				<!-- Footer info -->
-				<div class="col-md-2">
-					<div class="footer-item">
-						<h4 class="text-link">Llama</h4>
-						<ul>
-							<li><a href="#">+52 (477) 152-31-20</a></li>
-							<li><a href="#">+52 (477) 211-15-95, 96, 97</a></li>
-						</ul>
-					</div>
-				</div>
-				<!-- Footer info end-->
-				<!-- Footer info -->
-				<div class="col-md-2">
-					<div class="footer-item">
-						<h4 class="text-link">Escribe</h4>
-						<ul>
-							<li><a href="#">yourmail@domain.com</a></li>
-							<li><a href="#">email@website.com</a></li>
-						</ul>
-					</div>
-				</div>
-				<!-- Footer info-->
-				<!-- Footer info end-->
-				<div class="col-md-2">
-					<div class="footer-item">
-						<h4 class="text-link">Visita</h4>
+<?php //if (!is_post_type_archive('proyectos') && !is_singular('proyectos') && !is_post_type_archive('desarrollos') && !is_singular('desarrollos') && !is_404()):  ?>
+<?php if (!is_post_type_archive('proyectos') && !is_post_type_archive('desarrollos') && !is_404()): ?>
+    <!-- content footer-->
+    <div class="height-emulator"></div>
+    <footer class="content-footer">
+        <!--  container  -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <!-- Footer logo -->
+                    <!--<div class="footer-item footer-logo">
+    <a href="index.html"><img src="images/footer-logo.png" alt=""></a>
+    <p> </p>
+    </div>-->
+                    <!-- Footer logo end -->
+                </div>
+                <!-- Footer info -->
+                <div class="col-md-2">
+                    <div class="footer-item">
+                        <h4 class="text-link">Llama</h4>
+                        <ul>
+                            <li><a href="#">+52 (477) 152-31-20</a></li>
+                            <li><a href="#">+52 (477) 211-15-95, 96, 97</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Footer info end-->
+                <!-- Footer info -->
+                <div class="col-md-2">
+                    <div class="footer-item">
+                        <h4 class="text-link">Escribe</h4>
+                        <ul>
+                            <li><a href="#">yourmail@domain.com</a></li>
+                            <li><a href="#">email@website.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Footer info-->
+                <!-- Footer info end-->
+                <div class="col-md-2">
+                    <div class="footer-item">
+                        <h4 class="text-link">Visita</h4>
 
 
 
 
-						<ul>
-							<li>Av. Cerro Gordo 130 Int. 5001
-								<br>Col. Cerro Gordo
-								<br>C.P. 37129
-								<br>León Guanajuato</li>
-							<li> <a href="" target="_blank">Ver mapa</a></li>
-						</ul>
-					</div>
-				</div>
-				<!-- Footer info end-->
-			</div>
-			<!-- Footer copyright -->
-			<div class="row">
-				<div class="col-md-6"></div>
-				<div class="col-md-6">
-					<div class="footer-wrap">
-						<span class="copyright">  &#169; Aryba   2016.  All rights reserved. power by <a href="#" style="color:#fff;">unusual-projects.com</a>
-						</span>
-						<span class="to-top">Arriba</span>
-					</div>
-				</div>
-			</div>
-			<!-- Footer copyright end -->
-		</div>
-		<!--  container  end -->
-		<!-- Hover animation  -->
-		<canvas class="particular"></canvas>
-		<!-- Hover animation  end -->
-	</footer>
-	<!-- content footer end -->
-	<?php endif; ?>
-		<!-- share  -->
-		<div class="share-inner">
-			<div class="share-container  isShare" data-share="['facebook','googleplus','twitter','linkedin']"></div>
-			<div class="close-share"></div>
-		</div>
-		<!-- share end -->
-		</div>
-		<!-- content-holder  end-->
+                        <ul>
+                            <li>Av. Cerro Gordo 130 Int. 5001
+                                <br>Col. Cerro Gordo
+                                <br>C.P. 37129
+                                <br>León Guanajuato</li>
+                            <li> <a href="" target="_blank">Ver mapa</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Footer info end-->
+            </div>
+            <!-- Footer copyright -->
+            <div class="row">
+                <div class="col-md-6"></div>
+                <div class="col-md-6">
+                    <div class="footer-wrap">
+                        <span class="copyright">  &#169; Aryba   2016.  All rights reserved. power by <a href="#" style="color:#fff;">unusual-projects.com</a>
+                        </span>
+                        <span class="to-top">Arriba</span>
+                    </div>
+                </div>
+            </div>
+            <!-- Footer copyright end -->
+        </div>
+        <!--  container  end -->
+        <!-- Hover animation  -->
+        <canvas class="particular"></canvas>
+        <!-- Hover animation  end -->
+    </footer>
+    <!-- content footer end -->
+<?php endif; ?>
+<!-- share  -->
+<dixv class="share-inner">
+    <div class="share-container  isShare" data-share="['facebook','googleplus','twitter','linkedin']"></div>
+    <div class="close-share"></div>
+</div>
+<!-- share end -->
+</div>
+<!-- content-holder  end-->
 
-		</div>
-		<!-- wrapper end -->
-		<!-- Fixed footer -->
-		<footer class="fixed-footer">
-			<div class="footer-social">
-				<ul>
-					<li><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
-					<li><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
-					<li><a href="#" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-				</ul>
-			</div>
-			<!-- Header  title -->
-			<div class="footer-title">
-				<h2><a  href="#"></a></h2>
-			</div>
-			<!-- Header  title  end-->
-		</footer>
+</div>
+<!-- wrapper end -->
+<!-- Fixed footer -->
+<footer class="fixed-footer">
+    <div class="footer-social">
+        <ul>
+            <li><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+        </ul>
+    </div>
+    <!-- Header  title -->
+    <div class="footer-title">
+        <h2><a  href="#"></a></h2>
+    </div>
+    <!-- Header  title  end-->
+</footer>
 
-		<!-- Fixed footer end-->
-		</div>
-		<!-- Main end -->
-		<!--=============== google map ===============-->
-		
+<!-- Fixed footer end-->
+</div>
+<!-- Main end -->
+<!--=============== google map ===============-->
 
-		<!--=============== scripts  ===============-->
-		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js"></script>
-		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
-		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
-                <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.validate.min.js"></script>
-                
-                <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-                <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/map.js"></script>
+
+<!--=============== scripts  ===============-->
 
 
 
-		<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
-			</body>
+</body>
 
-			</html>
+</html>
--- themes/aryba/header.php
+++ themes/aryba/header.php
@@ -9,75 +9,84 @@
  * @since Twenty Fifteen 1.0
  */
 ?>
-	<!DOCTYPE html>
-	<html <?php language_attributes(); ?> class="no-js">
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
 
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta charset="UTF-8">
-		<title>Aryba
-			<?php wp_title('|'); ?>
-		</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-		<meta name="robots" content="index, follow" />
-		<meta name="keywords" content="" />
-		<meta name="description" content="" />
-		<!--=============== css  ===============-->
-		<link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css">
-		<link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/plugins.css">
-		<link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
-		<!--=============== favicons ===============-->
-		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
-	</head>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta charset="UTF-8">
+        <title>Aryba
+            <?php wp_title('|'); ?>
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <!--=============== css  ===============-->
+        <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/plugins.css">
+        <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+        <!--=============== favicons ===============-->
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
 
-	<body <?php body_class(); ?>>
-		<!-- loader -->
-		<div class="loader">
-			<div id="movingBallG">
-				<div class="movingBallLineG"></div>
-				<div id="movingBallG_1" class="movingBallG"></div>
-			</div>
-		</div>
-		<!-- loader end -->
-		<!--================= main start ================-->
-		<div id="main">
-			<!--=============== header ===============-->
-			<header>
-				<!-- header-inner  -->
-				<div class="header-inner">
-					<!-- header logo -->
-					<div class="logo-holder">
-						<a href="<?php echo get_home_url();  ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt=""></a>
-					</div>
-					<!-- header logo end -->
-					<!-- mobile nav button -->
-					<div class="nav-button-holder">
-						<div class="nav-button vis-m"><span></span><span></span><span></span></div>
-					</div>
-					<!-- mobile nav button end -->
+        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js"></script>
+        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
+        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
+        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.validate.min.js"></script>
 
-					<!-- navigation  -->
-					<div class="nav-holder">
-						<nav>
-							<ul>
-								<li><a href="<?php echo get_home_url();  ?>/quienes-somos">Quienes Somos </a></li>
-								<li><a href="<?php echo get_home_url();  ?>/proyectos">Proyectos</a></li>
-								<li><a href="<?php echo get_home_url();  ?>/servicios">Servicios</a></li>
-								<li><a href="<?php echo get_home_url();  ?>/desarrollos">Desarollos</a></li>
-								<li><a href="<?php echo get_home_url();  ?>/socios">Socios</a></li>
-								<li><a href="<?php echo get_home_url();  ?>/premios">Premios</a></li>
-								<li><a href="<?php echo get_home_url();  ?>/contacto">Contacto</a></li>
-							</ul>
-						</nav>
-					</div>
-					<!-- navigation  end -->
-				</div>
-				<!-- header-inner  end -->
-				<!-- share button  -->
-				<div class="show-share isShare">
-					<span>Compartir</span>
-					<i class="fa fa-chain-broken"></i>
-				</div>
-				<!-- share  end-->
-			</header>
-			<div id="wrapper">
+        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/map.js"></script>
+
+    </head>
+
+    <body <?php body_class(); ?>>
+        <!-- loader -->
+        <div class="loader">
+            <div id="movingBallG">
+                <div class="movingBallLineG"></div>
+                <div id="movingBallG_1" class="movingBallG"></div>
+            </div>
+        </div>
+        <!-- loader end -->
+        <!--================= main start ================-->
+        <div id="main">
+            <!--=============== header ===============-->
+            <header>
+                <!-- header-inner  -->
+                <div class="header-inner">
+                    <!-- header logo -->
+                    <div class="logo-holder">
+                        <a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt=""></a>
+                    </div>
+                    <!-- header logo end -->
+                    <!-- mobile nav button -->
+                    <div class="nav-button-holder">
+                        <div class="nav-button vis-m"><span></span><span></span><span></span></div>
+                    </div>
+                    <!-- mobile nav button end -->
+
+                    <!-- navigation  -->
+                    <div class="nav-holder">
+                        <nav>
+                            <ul>
+                                <li><a href="<?php echo get_home_url(); ?>/quienes-somos">Quienes Somos </a></li>
+                                <li><a href="<?php echo get_home_url(); ?>/proyectos">Proyectos</a></li>
+                                <li><a href="<?php echo get_home_url(); ?>/servicios">Servicios</a></li>
+                                <li><a href="<?php echo get_home_url(); ?>/desarrollos">Desarollos</a></li>
+                                <li><a href="<?php echo get_home_url(); ?>/socios">Socios</a></li>
+                                <li><a href="<?php echo get_home_url(); ?>/premios">Premios</a></li>
+                                <li><a href="<?php echo get_home_url(); ?>/contacto">Contacto</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <!-- navigation  end -->
+                </div>
+                <!-- header-inner  end -->
+                <!-- share button  -->
+                <div class="show-share isShare">
+                    <span>Compartir</span>
+                    <i class="fa fa-chain-broken"></i>
+                </div>
+                <!-- share  end-->
+            </header>
+            <div id="wrapper">
